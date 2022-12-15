a01b966b498169fcb8dca1b77c89d76ebe58343d
Asce <wuhejin0512@163.com>
2022-12-15 22:04:33 +0800

    add user entity

---
+++ src/main/java/com/huaxing/entity/TbUser.java
@@ -0,0 +1,69 @@
+package com.huaxing.entity;
+
+import java.util.Date;
+
+public class TbUser {
+    private Integer id;
+    private String username;
+    private String password;
+    private String phone;
+    private String gender;
+    private Date createTime;
+    private Date updateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
